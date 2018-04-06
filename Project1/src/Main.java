90a660e24f1d2dfb89230a4cfe396735cbc7be1d
UWJimP <phanjim2@hotmail.com>
2018-04-05 18:45:32 -0700

    Initial Commit

--- Project1/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main {
-
-	public static void main(String[] args) {
-		System.out.println("Go team dirty water!");
-		System.out.println("Bwahahaha I messed with the code! -Jim");
-	}
-
-}
